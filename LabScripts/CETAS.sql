8ec78d21408fff74e1fb351288dd3d68d5f2b4ce
Sree Ram <sreekanth_bhr@yahoo.com>
2023-03-02 09:09:06 -0600

    Add files via upload

---
+++ LabScripts/CETAS.sql
@@ -0,0 +1,24 @@
+USE DataExplorationDB;
+GO
+
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Lz8oq1dn';
+
+CREATE DATABASE SCOPED CREDENTIAL [WorkspaceIdentity] WITH IDENTITY = 'Managed Identity';
+GO
+
+CREATE EXTERNAL FILE FORMAT parquet_file_format
+WITH
+(  
+    FORMAT_TYPE = PARQUET,
+    DATA_COMPRESSION = 'org.apache.hadoop.io.compress.SnappyCodec'
+);
+GO
+
+
+CREATE EXTERNAL TABLE [dbo].taxideltatable WITH (
+        LOCATION = '/data/curatedparquet1/',
+        DATA_SOURCE = ContosoLake,
+        FILE_FORMAT = parquet_file_format
+) AS
+SELECT * FROM dbo.GreenTaxiCleansed;
+GO
